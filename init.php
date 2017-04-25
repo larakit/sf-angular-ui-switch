a9b294000a6c288caca1b97632cf45e9fc43c74b
Alexey Berdnikov <aberdnikov@gmail.com>
2017-04-25 10:03:51 +0500

    fixed

---
+++ init.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Larakit.
+ * Link: http://github.com/larakit
+ * User: Alexey Berdnikov
+ * Date: 25.04.17
+ * Time: 10:02
+ */
+\Larakit\StaticFiles\Manager::package('larakit/sf-angular-ui-switch')
+    ->usePackage('larakit/sf-angular')
+    ->setSourceDir('public')
+    ->jsPackage('angular-ui-switch.js')
+    ->cssPackage('angular-ui-switch.css')
+;
